7a5eda659b30468f7e769d6feed24f900bacbcf3
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-30 11:56:45 -0900

    add background task func

--- pkgs/server/server.go
+++ pkgs/server/server.go
@@ -2,13 +2,33 @@ package server
 
 import (
 	"net/http"
+	"sync"
 )
 
 type Server struct {
 	Host string
 	Port string
+	Wg   *sync.WaitGroup
+}
+
+func NewServer(host, port string) *Server {
+	return &Server{
+		Host: host,
+		Port: port,
+		Wg:   &sync.WaitGroup{},
+	}
 }
 
 func (s *Server) Start(router http.Handler) error {
 	return http.ListenAndServe(s.Host+":"+s.Port, router)
+}
+
+// Background starts a go routine that runs on the servers pool. In the event of a shutdown
+// request, the server will wait until all open goroutines have finished before shutting down.
+func (s *Server) Background(task func()) {
+	s.Wg.Add(1)
+	go func() {
+		defer s.Wg.Done()
+		task()
+	}()
 }
