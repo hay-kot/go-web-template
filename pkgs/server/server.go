678f016f9d43b32daf700ec8a05cf25039039ff7
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-01-31 09:55:17 -0900

    implement graceful shutdown for http handlers

--- pkgs/server/server.go
+++ pkgs/server/server.go
@@ -1,8 +1,15 @@
 package server
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 )
 
 // TODO: #3 Implement graceful shutdown
@@ -23,7 +30,52 @@ func NewServer(host, port string) *Server {
 }
 
 func (s *Server) Start(router http.Handler) error {
-	return http.ListenAndServe(s.Host+":"+s.Port, router)
+	httpServer := &http.Server{
+		Addr:         s.Host + ":" + s.Port,
+		Handler:      router,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	shutdownError := make(chan error)
+
+	go func() {
+		// Create a quit channel which carries os.Signal values.
+		quit := make(chan os.Signal, 1)
+
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
+		// relay them to the quit channel.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. block until received
+		s := <-quit
+		fmt.Println("received termination request -> ", s.String())
+
+		// Create a context with a 5-second timeout.
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutdownError <- httpServer.Shutdown(ctx)
+
+		// Exit the application with a 0 (success) status code.
+		os.Exit(0)
+	}()
+
+	err := httpServer.ListenAndServe()
+
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Server shutdown successfully")
+
+	return nil
 }
 
 // Background starts a go routine that runs on the servers pool. In the event of a shutdown
