5ca850342473d40fce7a314a5f17be824e38c870
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-02-02 10:26:51 -0900

    refactor config

--- client/test/config/index.ts
+++ client/test/config/index.ts
@@ -1,18 +1,4 @@
-interface Config {
-  PORT: string;
-  HOST: string;
-  BASE_URL: string;
-}
+export const PORT = "3915";
+export const HOST = "http://127.0.0.1";
+export const BASE_URL = HOST + ":" + PORT;
 
-class Configuration implements Config {
-  PORT: string;
-  HOST: string;
-  BASE_URL: string;
-  constructor() {
-    (this.PORT = process.env.API_WEB_PORT || "3000"),
-      (this.HOST = "http" + process.env.API_WEB_HOST || "http://127.0.0.1"),
-      (this.BASE_URL = this.HOST + ":" + this.PORT);
-  }
-}
-
-export const config = new Configuration();
