147120922f216ecc7fca715c9515a00eab4da52b
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-02-02 15:21:55 -0900

    refactor for vitest

--- client/test/setup.ts
+++ client/test/setup.ts
@@ -1,19 +1,20 @@
 import { exec } from "child_process";
 import * as config from "./config";
 
-before(() => {
+export const setup = () => {
   console.log("Starting Client Tests");
   console.log({
     PORT: config.PORT,
     HOST: config.HOST,
     BASE_URL: config.BASE_URL,
   });
-});
+};
 
-after(() => {
-  const pc = exec("pkill -SIGTERM api"); // Kill background API process
-
-  pc.stdout.on("data", (data) => {
-    console.log(`stdout: ${data}`);
-  });
-});
+export const teardown = () => {
+  if (process.env.TEST_SHUTDOWN_API_SERVER) {
+    const pc = exec("pkill -SIGTERM api"); // Kill background API process
+    pc.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+  }
+};
--- client/test/v1/base.test.ts
+++ client/test/v1/base.test.ts
@@ -1,14 +1,27 @@
-import request from "supertest";
+import { getClient } from "../../client";
+import { describe, it, expect } from "vitest";
 import * as config from "../config";
 
-const api = request(config.BASE_URL);
+const client = getClient(config.BASE_URL, "v1");
 
-describe("GET /api", function () {
-  it("responds with json", function (done) {
-    api
-      .get("/api")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
+describe("GET /api/status", function () {
+  it("server is available", async function (done) {
+    try {
+      const res = await client.status();
+
+      expect(res.status).toBe(200);
+      expect(res.statusText).toBe("OK");
+      expect(res.headers.get("content-type")).toBe("application/json");
+
+      const body = await res.json();
+      expect(body).toEqual({
+        status: "available",
+        message: "The service is ready to use",
+      });
+
+      done();
+    } catch (err) {
+      done(err);
+    }
   });
 });
