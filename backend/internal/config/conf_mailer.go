d7708e09fc2ad2f235bac43165d0de7db2b86241
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-02-03 21:27:08 -0900

    add config tests

--- backend/internal/config/conf_mailer.go
+++ backend/internal/config/conf_mailer.go
@@ -11,5 +11,5 @@ type MailerConf struct {
 // Ready is a simple check to ensure that the configuration is not empty.
 // or with it's default state.
 func (mc *MailerConf) Ready() bool {
-	return mc.Host != "" && mc.Port != 0 && mc.Username != "" && mc.Password != ""
+	return mc.Host != "" && mc.Port != 0 && mc.Username != "" && mc.Password != "" && mc.From != ""
 }
